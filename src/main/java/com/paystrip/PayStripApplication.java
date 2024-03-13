f0b84fdc5530c9c394d3cdb1ad609aef306eb892
bankimmm <bankimmm474@gmail.com>
2024-03-13 23:05:52 +0530

    some message

---
+++ src/main/java/com/paystrip/PayStripApplication.java
@@ -0,0 +1,13 @@
+package com.paystrip;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PayStripApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PayStripApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/paystrip/PayStripApplicationTests.java
@@ -0,0 +1,13 @@
+package com.paystrip;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PayStripApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
